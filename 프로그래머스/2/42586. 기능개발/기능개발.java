66d7fed7f12c10f124137786b6b4919116bfda84
Youkamii <gkfkd747@naver.com>
2024-06-03 12:00:00 +0900

    .

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -5,7 +5,7 @@ class Solution {
         List<Integer> answerList = new ArrayList<>();
 		int[] jobDone = new int[progresses.length];
 		int a = 1;
-		int b = 1;
+		int b = 0;
 
 		for (int i = 0; i < progresses.length; i++)
 			jobDone[i] = ((100 - progresses[i] - 1) / speeds[i] + 1);
