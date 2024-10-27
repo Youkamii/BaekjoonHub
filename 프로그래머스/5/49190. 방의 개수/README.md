0fa0082de88838f7252af21729a318b2fd5ae787
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2024-10-27 18:06:55 +0900

    [level 5] Title: 방의 개수, Time: 493.04 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/5/49190. 방의 개수/README.md
+++ 프로그래머스/5/49190. 방의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 5] 방의 개수 - 49190 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49190?language=swift) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49190?language=javascript) 
 
 ### 성능 요약
 
-메모리: 46.8 MB, 시간: 320.88 ms
+메모리: 106 MB, 시간: 493.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 27일 18:00:04
+2024년 10월 27일 18:06:58
 
 ### 문제 설명
 
---
+++ 프로그래머스/5/49190. 방의 개수/방의 개수.js
@@ -0,0 +1,48 @@
+function solution(arrows) {
+    const direction = [
+        [0, 2],
+        [2, 2], 
+        [2, 0],
+        [2, -2],
+        [0, -2],
+        [-2, -2],
+        [-2, 0], 
+        [-2, 2],
+    ];
+
+    const visitedNodes = new Set();
+    const visitedEdges = new Set();
+
+    let x = 0, y = 0;
+    visitedNodes.add(`${x},${y}`);
+    let roomCount = 0;
+
+    for (let arrow of arrows) {
+        const dx = direction[arrow][0];
+        const dy = direction[arrow][1];
+
+        for (let i = 0; i < 2; i++) {
+            const nx = x + dx / 2;
+            const ny = y + dy / 2;
+
+            const currentNode = `${x},${y}`;
+            const nextNode = `${nx},${ny}`;
+            const edge = `${currentNode}->${nextNode}`;
+            const reverseEdge = `${nextNode}->${currentNode}`;
+
+            if (!visitedEdges.has(edge)) {
+                visitedEdges.add(edge);
+                visitedEdges.add(reverseEdge);
+                if (visitedNodes.has(nextNode)) {
+                    roomCount++;
+                }
+            }
+
+            visitedNodes.add(nextNode);
+            x = nx;
+            y = ny;
+        }
+    }
+
+    return roomCount;
+}
