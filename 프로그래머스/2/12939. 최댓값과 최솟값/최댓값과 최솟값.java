a5a22b4c34428c6471fa16243b6c7a6664a3a03e
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2024-10-27 17:41:16 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 12.66 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -2,7 +2,6 @@ class Solution {
     public String solution(String s) {
         int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
-
         String[] sc = s.split(" ");
         
         for (String ss : sc) {
