3f6e1d3815bbff2486b57d9334eaf744d8d78de0
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2024-10-29 00:05:07 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.17 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -4,12 +4,12 @@ class Solution {
         long a = 1;
 		long b = 1;
 		long tmp;
-		
+		    
 		for (int i = 1; i < n; i++) {
 			tmp = a;
 			a = (a + b) % 1234567;
 			b = tmp;
-		}
+		} 
 		
 		return a;
         
