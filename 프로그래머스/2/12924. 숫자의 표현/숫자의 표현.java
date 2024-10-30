f058b889010a4c0a2b8f0e500bde98468ee49580
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2024-10-31 00:06:53 +0900

    [level 2] Title: 숫자의 표현, Time: 1.74 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -11,7 +11,7 @@ class Solution {
                     break;
                 }
                 else if (sum > n) {
-                    break;
+                    break; 
                 }
             }
         }        
