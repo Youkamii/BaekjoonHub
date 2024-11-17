0848d9d0ef5167ccb36b3cc3550094a5aa66c048
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2024-11-17 12:31:04 +0900

    [Silver III] Title: 통계학, Time: 256 ms, Memory: 41180 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.java
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.*;
 
 class Main {
 
@@ -9,39 +8,40 @@ class Main {
         int n = Integer.parseInt(br.readLine());
 
         int[] cnt = new int[8001];
-        List<Integer> list = new ArrayList<>();
-        int sum = 0, max = -4000, min = 4000;
+        int sum = 0, max = -4000, min = 4000, maxCnt = 0, totalCount = 0;
+        int median = 0, mode = 0;
+        boolean secondMode = false;
 
         for (int i = 0; i < n; i++) {
             int num = Integer.parseInt(br.readLine());
             sum += num;
             cnt[num + 4000]++;
-            list.add(num);
             if (num > max) max = num;
             if (num < min) min = num;
         }
 
-        Collections.sort(list);
-        int med = list.get(n / 2);
-
-        int maxVal = 0, maxCnt = 0;
-        boolean sec = false;
-
         for (int i = 0; i < 8001; i++) {
             if (cnt[i] > maxCnt) {
                 maxCnt = cnt[i];
-                maxVal = i - 4000;
-                sec = false;
-            } else if (cnt[i] == maxCnt && !sec) {
-                maxVal = i - 4000;
-                sec = true;
+                mode = i - 4000;
+                secondMode = false;
+            } else if (cnt[i] == maxCnt && !secondMode) {
+                mode = i - 4000;
+                secondMode = true;
+            }
+
+            if (totalCount < n / 2 + 1) {
+                totalCount += cnt[i];
+                if (totalCount >= n / 2 + 1 && median == 0) {
+                    median = i - 4000;
+                }
             }
         }
 
         int avg = (int) Math.round((double) sum / n);
         int range = max - min;
 
-        bw.write(avg + "\n" + med + "\n" + maxVal + "\n" + range + "\n");
+        bw.write(avg + "\n" + median + "\n" + mode + "\n" + range + "\n");
         bw.flush();
     }
 }
