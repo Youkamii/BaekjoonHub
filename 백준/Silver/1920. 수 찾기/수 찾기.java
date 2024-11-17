bec61c859bce762cb48a04443343d025a9bbdbdd
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2024-11-17 09:52:22 +0900

    [Silver IV] Title: 수 찾기, Time: 460 ms, Memory: 60428 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,5 +1,11 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 class Main {
     static int stoi(String s) {
