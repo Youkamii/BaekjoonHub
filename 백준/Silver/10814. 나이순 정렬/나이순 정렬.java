9e5d16084c5661a70bdfb13cf2652fd2f6c55082
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2024-11-17 09:54:50 +0900

    [Silver V] Title: 나이순 정렬, Time: 316 ms, Memory: 39640 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,5 +1,9 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 class Main {
     
