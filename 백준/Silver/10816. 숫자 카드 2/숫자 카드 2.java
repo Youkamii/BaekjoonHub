627bd3911a4c04cce8aee5aaf8ac97224467e418
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2024-11-17 11:06:36 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 760 ms, Memory: 254420 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -2,35 +2,38 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static int stoi (String s) {
-        return Integer.parseInt(s);
-    }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
-        Map<Integer, Integer> map = new HashMap<>();
+        int[] parray = new int[10000001];
+        int[] marray = new int[10000001];
 
-        int n = stoi(br.readLine());
-        st = new StringTokenizer (br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            int key = stoi(st.nextToken());
-            map.put(key, map.getOrDefault(key, 0) + 1);
+            int key = Integer.parseInt(st.nextToken());
+            if (key >= 0)
+                parray[key] += 1;
+            else
+                marray[-key] += 1;
         }
 
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
 
-        int m = stoi(br.readLine());
-        st = new StringTokenizer (br.readLine());
         for (int i = 0; i < m; i++) {
-            int key = stoi(st.nextToken());
-            sb.append(map.getOrDefault(key,  0)).append(' ');
+            int key = Integer.parseInt(st.nextToken());
+            if (key >= 0)
+                sb.append(parray[key]).append(" ");
+            else
+                sb.append(marray[-key]).append(" ");
         }
 
         bw.write(sb.toString());
         bw.flush();
-
     }
 }
