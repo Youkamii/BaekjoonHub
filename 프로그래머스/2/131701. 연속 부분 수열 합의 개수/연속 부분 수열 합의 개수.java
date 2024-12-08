7546bd700c70c1a4b0dd8a61907ca377c1e133fe
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2024-12-09 01:10:13 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 410.55 ms, Memory: 139 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+
+class Solution {
+    public int solution(int[] elements) {
+        Set<Integer> answer = new HashSet<>();
+		int Length = elements.length;
+		int[] doubleElements = new int[Length * 2]; 
+
+		for (int i = 0; i < Length * 2; i++)
+			doubleElements[i] = elements[i % Length];
+
+		for (int cases = 1; cases <= Length; cases++) {
+			for (int i = 0; i < Length * 2 - cases; i++) {
+				int sum = 0;
+				for (int j = i; j < i + cases; j++) {
+					sum += doubleElements[j];
+				}
+				answer.add(sum);
+			}
+		}
+		return answer.size();
+    }
+}
