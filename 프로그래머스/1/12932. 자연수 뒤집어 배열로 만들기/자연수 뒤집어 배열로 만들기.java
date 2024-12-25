29b235a7440c29841ccc8c38ae583415b74679f0
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2024-12-25 15:19:23 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 6.71 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -1,21 +1,14 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(long n) {
-        int lengthNum = 1;
-		long tmp = n; 
-
-		for (int i = 0; 0 != (tmp /= 10) ; i++){ lengthNum++; }
-
-		int[] answer = new int[lengthNum]; 
-
-		for (int i = 0; i < lengthNum ; i++ ) {
-
-			if (10 < n)
-				answer[i] = (int)(n % 10);
-			else
-				answer[i] = (int)n;
-			n /= 10;
-		}
-
-		return answer;
-	}
+        List<Integer> answer = new ArrayList<>();
+        String str = String.valueOf(n);
+        
+        for (int i = str.length() - 1; i >= 0; i--) {
+            answer.add(Character.getNumericValue(str.charAt(i)));
+        }
+        
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
 }
