0deb3c0b01a8ccb6691286a8d29a68a8bd6d6662
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2024-12-25 15:40:22 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,12 +1,19 @@
 class Solution {
-    public int solution(int num) {
-        long tmp = num;
-		 
-		for (int i = 0; i < 500; i++) {
-			if (tmp == 1)
-                return i;
-            tmp = tmp % 2 == 0 ? tmp / 2 : tmp * 3 + 1;
-		}
-		return -1;
+    public int solution(int n) {
+        long num = n;
+        if (num == 1) return 0;
+        for (int i = 0; i < 500; i++) {
+            if (num % 2 == 0)
+                num /= 2;
+            else
+                num = num * 3 + 1;
+            
+            if (num == 1) return i + 1;
+        }
+        
+        return -1;
+        
+
+        
     }
 }
