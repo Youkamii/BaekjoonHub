7cc192984aafe77c2bc7309c0c3d82af975f893d
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2024-12-25 15:24:54 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.07 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Solution {
+    public long solution(long n) {
+        long sqrt = (long) Math.sqrt(n);
+        return sqrt * sqrt == n ? (sqrt + 1) * (sqrt + 1) : - 1;
+    }
+}
