0bc60aeed8c734712dc000cd4eed103fb927820a
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2025-01-04 00:12:32 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.04 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -1,5 +1,3 @@
 class Solution {
-    public String solution(String my_string, String letter) {
-        return my_string.replace(letter, "");
-    }
+    public String solution(String my_string, String letter) { return my_string.replace(letter, ""); }
 }
