1c4d5e45129e499e832b1eb5d5f2451270378b89
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2025-01-23 18:28:00 +0900

    [level 1] Title: 평균 구하기, Time: 0.29 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.cs
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.cs
@@ -2,12 +2,9 @@ using System;
 
 public class Solution {
     public double solution(int[] arr) {
-        double sum = 0; 
-        
-        foreach (int num in arr) {
-            sum += num;
-        }
-        
+        double sum = 0;         
+        foreach (int num in arr)
+            sum += num;     
         return sum / arr.Length;
     }
 }
