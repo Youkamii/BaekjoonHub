f93770efbece7cbf630f93ec6790929cf75db470
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2025-02-14 13:06:47 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -1,10 +1,5 @@
 class Solution {
-    public int solution(int s, int n) {        
-        int i = 1;        
-        while(true) {
-            if (i * s / n > 0) 
-                return i;        
-            i++;
-        }   
+    public int solution(int s, int n) {
+        return (n + s - 1) / s;
     }
 }
