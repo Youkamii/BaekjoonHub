2b666fa041b4b20ec485727054ac6fb9e1563308
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2025-02-15 23:29:57 +0900

    [level 0] Title: l로 만들기, Time: 24.40 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -1,14 +1,5 @@
 class Solution {
-    public StringBuilder solution(String s) {
-        StringBuilder sb = new StringBuilder();
-        
-        for (char c : s.toCharArray()){
-            if (c >= 'a' && c < 'l')
-                sb.append("l");
-            else
-                sb.append(c);
-        }
-        
-        return sb;
+    public String solution(String myString) {
+        return myString.replaceAll("[^l-z]", "l");
     }
 }
