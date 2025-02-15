6467ae00293997d959e1f51a7140ed18d229deec
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2025-02-15 23:21:45 +0900

    [level 0] Title: 문자열 돌리기, Time: 181.53 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -1,4 +1,4 @@
-import java.util.*;
+/* import java.util.*;
 import java.io.*;
 
 public class Solution {
@@ -10,5 +10,16 @@ public class Solution {
         for (int i = 0; i < s.length(); i++)
             bw.write(s.charAt(i) + "\n");
         bw.flush();
+    }
+} */
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        for (char ch : a.toCharArray())
+            System.out.println(ch);
     }
 }
