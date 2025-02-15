dc10049f63f9fcf86492bd4679e2bdde06872abd
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2025-02-15 22:57:01 +0900

    [level 0] Title: 카운트 업, Time: 0.02 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int s, int e) {
+        int l = e - s + 1;
+        int[] answer = new int[l];
+        
+        for (int i = 0; i < l; i++) answer[i] = i + s;        
+        
+        return answer;
+    }
+}
