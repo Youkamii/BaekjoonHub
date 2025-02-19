a67080525bb24f99ef084cf0ce6077e2d028b29e
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2025-02-19 21:26:35 +0900

    [level 0] Title: 특수문자 출력하기, Time: 146.32 ms, Memory: 70.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        bw.write("!@#$%^&*(\\'\"<>?:;");
+        bw.flush();
+    }
+}
