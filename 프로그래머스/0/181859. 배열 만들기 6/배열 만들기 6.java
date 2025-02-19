b8b5e6544787aec54525f0dfd35aecf76b65bc11
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2025-02-19 22:05:28 +0900

    [level 0] Title: 배열 만들기 6, Time: 52.70 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -3,20 +3,16 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] arr) {
         Stack<Integer> stack = new Stack<>();
-        int i = 0;
+        int i = -1;
         
-        while(i < arr.length) {
-            if (stack.isEmpty()) 
-                stack.push(arr[i++]);
-            else if (!stack.isEmpty()) {
-                int sp = stack.peek();
-                int ci = arr[i++];
-                
-                if (sp == ci) stack.pop();
-                else stack.push(ci);
-            }            
-        }
-        
+        while(++i < arr.length) {
+            if (stack.isEmpty()) {
+                stack.push(arr[i]);                
+            } else {                
+                if (stack.peek() == arr[i]) stack.pop();
+                else stack.push(arr[i]);                
+            }
+        }        
         return stack.isEmpty() ? new int[]{-1} : stack.stream().mapToInt(Integer::intValue).toArray();
     }
 }
