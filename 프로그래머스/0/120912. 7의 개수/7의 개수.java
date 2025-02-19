cb26106de704d45a6eb932e2a90dc2c053d04d72
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2025-02-19 17:17:04 +0900

    [level 0] Title: 7의 개수, Time: 0.06 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        
+        for (int i : array) {
+            String str = String.valueOf(i);
+            for (char c : str.toCharArray())
+                if (c == '7') answer++;
+        }
+        
+        return answer;
+    }
+}
