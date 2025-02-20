094f07433e3d5b24b98c894339c76b921d6dedda
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2025-02-20 17:57:23 +0900

    [level 0] Title: 두 수의 합, Time: 441.53 ms, Memory: 125 MB -BaekjoonHub

---
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.math.BigInteger;
+
+class Solution {
+    public String solution(String a, String b) {
+        return new BigInteger(a).add(new BigInteger(b)).toString();
+    }
+}
