ff81c14107a2a23899c472554cea4e89abe5d2a2
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2025-02-21 11:36:59 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 4.18 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -1,15 +1,24 @@
 import java.util.*;
 
+class 학생 {
+    int number;
+    int rank;
+    
+    학생 (int number, int rank) {
+        this.number = number;
+        this.rank = rank;
+    }
+}
+
 class Solution {
     public int solution(int[] rank, boolean[] attendance) {
-        int answer = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < rank.length; i++)
-            if (attendance[i]) map.put(i, rank[i]);
-        
-        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
-        list.sort(Map.Entry.comparingByValue());      
+        List<학생> list = new ArrayList<>();
 
-        return 10000 * list.get(0).getKey() + 100 * list.get(1).getKey() + list.get(2).getKey();
+        for (int i = 0; i < rank.length; i++)
+            if (attendance[i]) list.add(new 학생(i, rank[i]));        
+
+        list.sort(Comparator.comparingInt(p -> p.rank));
+
+        return 10000 * list.get(0).number + 100 * list.get(1).number + list.get(2).number;
     }
 }
