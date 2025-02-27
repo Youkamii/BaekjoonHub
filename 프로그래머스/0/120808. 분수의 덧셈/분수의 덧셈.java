0af4288513803d99f6ad253d235f2a3797dad7f1
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2025-02-27 16:19:50 +0900

    [level 0] Title: 분수의 덧셈, Time: 5.56 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+        int u = denom1 * numer2 + numer1 * denom2;
+        int d = denom1 * denom2;
+        int m = Math.min(u, d);
+        
+        for (int i = 2; i <= m / 2; i++) {
+            while(u % i == 0 && d % i == 0) {
+                u /= i;
+                d /= i;
+            }
+        }
+
+        return new int[]{u, d};
+    }
+}
