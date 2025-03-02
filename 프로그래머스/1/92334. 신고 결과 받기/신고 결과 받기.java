cebfbdd0a99563a9eadbd108a3726db7a7e7166a
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2025-03-02 21:35:57 +0900

    [level 1] Title: 신고 결과 받기, Time: 274.35 ms, Memory: 169 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -3,24 +3,22 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        Map<String, User79> user = new HashMap<>();
+        Map<String, User> user = new HashMap<>();
 		int[] answer = new int[id_list.length];
-		//Map k v
 
 		for (String s : id_list) {
-			User79 nullList = new User79();
+			User nullList = new User();
 			user.put(s, nullList);
 		}
 		for (String s : report) {
 			String[] reporting = s.split(" ");
 
-			String reporte_R_Id = reporting[0]; // 신고자
-			String reporte_D_Id = reporting[1]; // 신고당한 사람
+			String reporte_R_Id = reporting[0];
+			String reporte_D_Id = reporting[1];
 
-			User79 reporter = user.get(reporte_R_Id);
-			User79 reported = user.get(reporte_D_Id);
+			User reporter = user.get(reporte_R_Id);
+			User reported = user.get(reporte_D_Id);
 
-			// 중복신고
 			if (!reporter.reportUser.contains(reporte_D_Id)) {
 				reporter.reportUser.add(reporte_D_Id);
 				reported.whoReportMe.add(reporte_R_Id);
@@ -28,10 +26,10 @@ class Solution {
 		}
 
 		for (int i = 0; i < answer.length; i++) {
-			User79 mailedUser = user.get(id_list[i]);
+			User mailedUser = user.get(id_list[i]);
 
 			for (String reportedId : mailedUser.reportUser) {
-				User79 reported = user.get(reportedId); 
+				User reported = user.get(reportedId); 
 
 				if (reported.whoReportMe.size() >= k)
 					answer[i]++;
@@ -44,11 +42,11 @@ class Solution {
 		return answer;
     }
     
-    class User79 {
+    class User {
 		List<String> reportUser;
 		List<String> whoReportMe;
 
-    	public User79() {
+    	public User() {
 			this.reportUser = new ArrayList<>();
 			this.whoReportMe = new ArrayList<>();
 		}
