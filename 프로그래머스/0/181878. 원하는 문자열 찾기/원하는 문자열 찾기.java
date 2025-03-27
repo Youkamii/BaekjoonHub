e01c45b48b9d999309f7cb6a45da2a3105f0acd9
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2025-03-27 17:09:02 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 8.06 ms, Memory: 95 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -1,5 +1,5 @@
 class Solution {
     public int solution(String myString, String pat) {
-        return myString.toUpperCase().contains(pat.toUpperCase()) ? 1 : 0;
+        return myString.toUpperCase().contains(pat.toUpperCase()) ? 1 : 0; 
     }
 }
