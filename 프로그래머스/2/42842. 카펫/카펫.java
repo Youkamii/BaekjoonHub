333ceb6a1e3b58c5b608f7651fb1c4ccac2a02ee
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2025-03-31 14:42:10 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,11 +1,11 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int x = brown / 2 - 3;
-        
-        for (int y = 1; y <= x; y++) {
-            if (x * y == yellow) return new int[]{x + 2, y + 2};
-            x--;
-        }
-        return null;
+		int x = (brown / 2) - 3;
+
+		for (int y = 1; y < x + 1; y++) {
+			if ((x * y) == yellow) return new int[]{x + 2,y + 2};
+			x--;
+		}
+		return null;
     }
 }
