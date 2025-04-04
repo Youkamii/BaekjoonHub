f1ca9f46126dbff0ac7be0ff1d511db030b1c5a9
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2025-04-04 15:11:48 +0900

    [level 2] Title: 할인 행사, Time: 79.71 ms, Memory: 120 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        
+		int answer = 0; 
+
+		Map<String, Integer> iWantIT = new HashMap<>();
+		for (int i = 0; i < want.length; i++)
+			iWantIT.put(want[i], number[i]); 
+
+		for (int i = 0; i <= discount.length - 10; i++) {
+			Map<String, Integer> discountSchedule = new HashMap<>();
+			for (int j = i; j < i + 10; j++)
+				discountSchedule.put(discount[j], discountSchedule.getOrDefault(discount[j], 0) + 1);
+
+			Boolean 지금이니 = true;
+
+			if (!iWantIT.equals(discountSchedule))
+				지금이니 = false;
+
+			if (지금이니)
+				answer++;
+		}
+
+		return answer;
+
+    }
+}
