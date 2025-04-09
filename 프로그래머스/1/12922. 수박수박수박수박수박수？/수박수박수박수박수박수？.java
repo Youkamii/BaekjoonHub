a3588b42bd0d66e61e253d08989aba2e6c92a105
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2025-04-10 05:01:03 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.52 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,9 +1,10 @@
 class Solution {
-    public String solution(int n) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            sb.append(i % 2 == 0 ? "수" : "박");
-        }
-        return sb.toString();
+    public StringBuilder solution(int n) {
+        StringBuilder answer = new StringBuilder();
+
+		for (int i = 0; i < n; i++)
+			answer.append((i % 2 == 0) ? "수" : "박");
+			
+		return answer;
     }
 }
