641ebf26d02b464276cb936f164d227999192337
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2025-04-16 08:20:29 +0900

    [level 2] Title: 올바른 괄호, Time: 18.10 ms, Memory: 54.6 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -7,7 +7,7 @@ class Solution {
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
             if (ch == '(') stack.push(ch);
-            else {
+            else { 
                 if (stack.isEmpty()) return false;                
                 stack.pop();
             }
