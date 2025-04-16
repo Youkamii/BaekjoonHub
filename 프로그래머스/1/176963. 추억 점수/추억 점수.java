63ae14aa0de13c29c0f9068fd1a242f82bb3efae
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2025-04-17 08:28:51 +0900

    [level 1] Title: 추억 점수, Time: 2.34 ms, Memory: 94.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < name.length; i++)
+            map.put(name[i], yearning[i]);
+        
+        int[] answer = new int[photo.length];
+        for (int i = 0; i < photo.length; i++) {
+            int sum = 0;
+            for (String j : photo[i]) {
+                if (map.containsKey(j))
+                    sum += map.get(j);
+            }
+            answer[i] = sum;
+        }
+        
+        return answer;
+    }
+}
