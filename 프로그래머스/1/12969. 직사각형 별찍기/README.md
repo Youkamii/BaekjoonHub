36e82c649845f2b5443dc2144e50b213d1314af8
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2025-04-21 08:31:12 +0900

    [level 1] Title: 직사각형 별찍기, Time: 212.54 ms, Memory: 59.8 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/README.md
+++ 프로그래머스/1/12969. 직사각형 별찍기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 직사각형 별찍기 - 12969 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12969) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12969?language=java) 
 
 ### 성능 요약
 
-메모리: 62.6 MB, 시간: 236.61 ms
+메모리: 59.8 MB, 시간: 212.54 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 25일 16:19:37
+2025년 04월 21일 08:31:09
 
 ### 문제 설명
 
--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.java
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.java
@@ -2,21 +2,19 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
+
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         
         for (int i = 0; i < m; i++) {
-            StringBuilder sb = new StringBuilder();
             for (int j = 0; j < n; j++)
-                sb.append("*");
-            bw.write(sb.toString());
-            bw.newLine();
-        }        
+                bw.write("*");
+            bw.write("\n");
+        }
         bw.flush();
     }
 }
