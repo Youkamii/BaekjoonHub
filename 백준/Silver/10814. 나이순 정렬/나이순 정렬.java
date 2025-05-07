59de1e95430f5e2a994385851cddf1918f9854bc
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2025-05-08 08:20:21 +0900

    [Silver V] Title: 나이순 정렬, Time: 232 ms, Memory: 31656 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,36 +1,39 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.io.*;
 
-class Main {
-    
-    public static void main (String[] args) throws IOException {
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 1) 입력 리더
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int n = Integer.parseInt(st.nextToken());
-        
-        StringBuilder[] Blist = new StringBuilder[201];
-        for (int i = 0; i < 201; i++) {
-            Blist[i] = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        // 2) 나이별 버킷 준비
+        StringBuilder[] buckets = new StringBuilder[201];
+        for (int i = 0; i <= 200; i++) {
+            buckets[i] = new StringBuilder();
         }
-        
+
+        // 3) 한 줄씩 읽어서 수동 파싱
         for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            int age = Integer.parseInt(st.nextToken());
-            
-            Blist[age].append(age).append(" ").append(st.nextToken()).append("\n");  
+            String line = br.readLine();
+            int space = line.indexOf(' ');
+            // 수동으로 나이 파싱 (substring 대신)
+            int age = 0;
+            for (int j = 0; j < space; j++) {
+                age = age * 10 + (line.charAt(j) - '0');
+            }
+            // 그대로 버킷에 쌓기 (line 안에 "age name" 형태)
+            buckets[age].append(line).append('\n');
         }
-        
-        for (int i = 0; i < 201; i++) {
-            bw.write(Blist[i].toString());
+        br.close();
+
+        // 4) 한 번에 출력
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder out = new StringBuilder();
+        for (StringBuilder sb : buckets) {
+            out.append(sb);
         }
-        
-		bw.flush();
+        bw.write(out.toString());
+        bw.flush();
+        bw.close();
     }
-    
 }
