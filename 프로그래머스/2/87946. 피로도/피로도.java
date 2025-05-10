61fcfb4e7f677a78d8dc8e610ecba2e14407e2dc
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2025-05-10 23:14:44 +0900

    [level 2] Title: 피로도, Time: 3.29 ms, Memory: 82.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,21 @@
+class Solution {
+    int Max = -1;
+    boolean[] visited;
+    
+    public int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+        dfs(k,dungeons, 0);     
+        return Max;
+    }
+    
+    void dfs (int k, int[][] dungeons, int count) {
+        Max = Max < count ? count : Max;
+        for (int i = 0; i < dungeons.length; i++) {
+            if (!visited[i] && k >= dungeons[i][0]) {
+                visited[i] = true;
+                dfs(k - dungeons[i][1], dungeons, count + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
