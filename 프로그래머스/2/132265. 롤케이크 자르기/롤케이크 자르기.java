c797f71424c038d74b18ce1ea35feb47ca268aa4
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2025-05-13 08:31:18 +0900

    [level 2] Title: 롤케이크 자르기, Time: 200.32 ms, Memory: 171 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {    
+    public int solution(int[] topping) {
+        int answer = 0;        
+        Map<Integer, Integer> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
+        
+        for (int t : topping)
+            map.put(t, map.getOrDefault(t, 0) + 1);
+        
+        for (int i = 0; i < topping.length; i++) {
+            int t = topping[i];
+            set.add(t);
+            map.put(t, map.get(t) - 1);
+            if(map.get(t) == 0) map.remove(t);
+            
+            if (set.size() == map.size()) answer++;
+            
+        }
+        
+        return answer;
+    }
+}
