609227870f10ed11bf497c37a3d1c450b6e4d659
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2025-05-20 08:26:24 +0900

    [level 2] Title: H-Index, Time: 8.49 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -5,7 +5,7 @@ class Solution {
         int answer = 0;
         int citationsLength = citations.length;
         Arrays.sort(citations);
-
+ 
         for (int i = 1; i <= citationsLength; i++) {
             int hIndex = 0;
             for (int j : citations) if (j >= i) hIndex++;
