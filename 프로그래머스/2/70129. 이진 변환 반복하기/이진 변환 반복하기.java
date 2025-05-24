1053a03093a2e08a6a9fe785206a0b2c3242c967
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2025-05-24 22:45:05 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 19.61 ms, Memory: 91.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(String s) {
+        int removeZero = 0;
+		int diversionCount = 0;
+
+		while (!s.equals("1")) {
+			diversionCount++; 
+
+			for (int i = 0; i < s.length(); i++) {
+				if (s.charAt(i) == '0')
+					removeZero++;
+			}
+			s = s.replace("0", "");
+			s = Integer.toBinaryString(s.length());
+		}
+
+		return new int[] {diversionCount, removeZero};
+    }
+}
