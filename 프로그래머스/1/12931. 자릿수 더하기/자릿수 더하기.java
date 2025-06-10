79ecf722b5bb517447525241819fb4c4ba22644b
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2025-06-10 22:49:34 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -3,11 +3,12 @@ import java.util.*;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        String str = String.valueOf(n);
-        
-        for (int i = 0; i < str.length(); i++)
-            answer += (int) (str.charAt(i) - '0');        
-        
+
+        while (n > 0) {
+            answer += n % 10;
+            n /= 10;
+        }
+
         return answer;
     }
 }
