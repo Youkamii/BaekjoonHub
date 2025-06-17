636bac8ab4601cc18f38c3aae24a366375d5d7cb
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2025-06-17 21:20:19 +0900

    [level 1] Title: 추억 점수, Time: 2.45 ms, Memory: 93.9 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -5,17 +5,43 @@ class Solution {
         Map<String, Integer> map = new HashMap<>();
         for (int i = 0; i < name.length; i++)
             map.put(name[i], yearning[i]);
-        
-        int[] answer = new int[photo.length];
-        for (int i = 0; i < photo.length; i++) {
+        int[] answer = new int[photo.length];        
+        for(int i = 0; i < photo.length; i++) {
             int sum = 0;
-            for (String j : photo[i]) {
-                if (map.containsKey(j))
-                    sum += map.get(j);
-            }
+            for (int j = 0; j < photo[i].length; j++)
+                sum += map.getOrDefault(photo[i][j], 0);
             answer[i] = sum;
-        }
-        
+        }        
         return answer;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int[] solution(String[] name, int[] yearning, String[][] photo) {
+//         Map<String, Integer> map = new HashMap<>();
+//         for (int i = 0; i < name.length; i++)
+//             map.put(name[i], yearning[i]);
+        
+//         int[] answer = new int[photo.length];
+//         for (int i = 0; i < photo.length; i++) {
+//             int sum = 0;
+//             for (String j : photo[i]) {
+//                 if (map.containsKey(j))
+//                     sum += map.get(j);
+//             }
+//             answer[i] = sum;
+//         }
+        
+//         return answer;
+//     }
+// }
