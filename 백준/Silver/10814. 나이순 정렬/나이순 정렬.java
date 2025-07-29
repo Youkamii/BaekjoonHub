d00ca63d803ed73dc00fd84586b42211a10aa067
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2025-07-29 19:32:34 +0900

    [Silver V] Title: 나이순 정렬, Time: 204 ms, Memory: 26480 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -2,25 +2,24 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(r.readLine());
 
-        StringBuilder[] buckets = new StringBuilder[201];
+        StringBuilder[] b = new StringBuilder[201];
         for (int i = 0; i <= 200; i++)
-            buckets[i] = new StringBuilder();
+            b[i] = new StringBuilder();
 
         for (int i = 0; i < n; i++) {
-            String line = br.readLine();
-            int space = line.indexOf(' ');
-            int age = 0;
-            for (int j = 0; j < space; j++) 
-                age = age * 10 + (line.charAt(j) - '0');
-            buckets[age].append(line).append('\n');
+            String l = r.readLine();
+            int s = l.indexOf(' ');
+            int a = 0;
+            for (int j = 0; j < s; j++) 
+                a = a * 10 + (l.charAt(j) - '0');
+            b[a].append(l).append('\n');
         }
 
-        StringBuilder out = new StringBuilder();
-        for (StringBuilder sb : buckets) bw.write(sb.toString());
-        bw.flush();
+        for (StringBuilder c : b) w.write(c.toString());
+        w.flush();
     }
 }
