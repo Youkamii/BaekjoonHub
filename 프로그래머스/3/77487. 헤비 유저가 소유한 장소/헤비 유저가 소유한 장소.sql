1997b7aafd76c5a38f64280a15440dbb85ddaebc
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2025-11-15 23:25:57 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -0,0 +1,13 @@
+SELECT
+    P.ID,
+    P.NAME,
+    P.HOST_ID
+FROM 
+    PLACES P
+WHERE 
+    P.HOST_ID IN (
+        SELECT HOST_ID
+        FROM PLACES
+        GROUP BY HOST_ID
+        HAVING COUNT(ID) > 1
+)
