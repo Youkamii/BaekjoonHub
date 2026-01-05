b0b4060fd61119ee157bcc65125d7de589268f98
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2026-01-05 22:01:06 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 68 ms, Memory: 11548 KB -BaekjoonHub

--- 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -5,12 +5,11 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = (Integer.parseInt(br.readLine()) / 4);
-        StringBuilder sb = new StringBuilder();
         
-        for(int i = 0; i < n; i++) sb.append("long ");
-        sb.append("int");
+        for(int i = 0; i < n; i++)
+            bw.write("long ");
+        bw.write("int");
         
-        bw.write(sb.toString());
         bw.flush();
     }
 }
