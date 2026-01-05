aa445aae28152f22baa1c7b83848ace36a3383a5
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2026-01-05 22:10:49 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 68 ms, Memory: 11452 KB -BaekjoonHub

--- 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -1,33 +1,15 @@
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int n = 0;
-        int c;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         
-        while ((c = System.in.read()) <= 32); 
+        for(int i = 1; i < n / 4; i++)
+            sb.append("long ");
+        sb.append("long int");
         
-        do {
-            n = (n << 3) + (n << 1) + (c - '0');
-        } while ((c = System.in.read()) > 32);
-
-        int loop = n / 4;
-
-        byte[] buffer = new byte[loop * 5 + 3];
-        int idx = 0;
-
-        for (int i = 0; i < loop; i++) {
-            buffer[idx++] = 108;
-            buffer[idx++] = 111;
-            buffer[idx++] = 110;
-            buffer[idx++] = 103;
-            buffer[idx++] = 32;
-        }
-
-        buffer[idx++] = 105;
-        buffer[idx++] = 110;
-        buffer[idx++] = 116;
-
-        System.out.write(buffer);
+        System.out.println(sb);
     }
 }
