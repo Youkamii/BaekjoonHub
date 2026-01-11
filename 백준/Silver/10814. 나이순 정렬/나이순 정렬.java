a83bcfb53f93dc592927f0480104577486a95ada
Youkam Jeong <87900502+Youkamii@users.noreply.github.com>
2026-01-11 19:07:13 +0900

    [Silver V] Title: 나이순 정렬, Time: 160 ms, Memory: 18652 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,25 +1,69 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws Exception {
+        Reader r = new Reader();
         BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(r.readLine());
 
+        int n = r.nextInt();
+        
         StringBuilder[] b = new StringBuilder[201];
-        for (int i = 0; i <= 200; i++)
+        String[] ageStr = new String[201];
+        
+        for (int i = 1; i <= 200; i++) {
             b[i] = new StringBuilder();
-
-        for (int i = 0; i < n; i++) {
-            String l = r.readLine();
-            int s = l.indexOf(' ');
-            int a = 0;
-            for (int j = 0; j < s; j++) 
-                a = a * 10 + (l.charAt(j) - '0');
-            b[a].append(l).append('\n');
+            ageStr[i] = i + " ";
         }
 
-        for (StringBuilder c : b) w.write(c.toString());
+        for (int i = 0; i < n; i++) {
+            int age = r.nextInt();
+            b[age].append(ageStr[age]);
+            r.appendName(b[age]);
+        }
+
+        for (int i = 1; i <= 200; i++) {
+            if (b[i] != null && b[i].length() > 0) {
+                w.write(b[i].toString());
+            }
+        }
         w.flush();
+        w.close();
+    }
+
+    static class Reader {
+        final int SIZE = 1 << 16;
+        byte[] buffer = new byte[SIZE];
+        int index, size;
+
+        int nextInt() throws Exception {
+            int n = 0;
+            byte c;
+            while ((c = read()) <= 32);
+            do {
+                n = (n << 3) + (n << 1) + (c & 15);
+            } while (isNumber(c = read()));
+            return n;
+        }
+
+        void appendName(StringBuilder sb) throws Exception {
+            byte c = read();
+            while (c > 32) {
+                sb.append((char)c);
+                c = read();
+            }
+            sb.append('\n');
+        }
+
+        boolean isNumber(byte c) {
+            return 47 < c && c < 58;
+        }
+
+        byte read() throws Exception {
+            if (index == size) {
+                size = System.in.read(buffer, index = 0, SIZE);
+                if (size == -1) return -1;
+            }
+            return buffer[index++];
+        }
     }
 }
